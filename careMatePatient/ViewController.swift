23c5cf134e4daf7a845fbd21a53d1bb294c54133
khabeer <khabeer@mostafaios.local>
2020-04-01 11:11:56 +0200

    Initial Commit

---
+++ careMatePatient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  careMatePatient
+//
+//  Created by khabeer on 4/1/20.
+//  Copyright © 2020 khabeer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
